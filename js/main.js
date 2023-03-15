72380be18fe41d6ecc7877f9c3d759ce3d3b3ff4
jingyig16 <jingyig616@gmail.com>
2023-03-15 15:06:51 -0400

    files

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Declare constants
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 500;
+const MARGINS = {left: 70, right: 70, top: 70, bottom: 70};
+
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+
+// Frame1: airport 
+const FRAME1 = d3.select("#length") 
+					.append("svg")
+						.attr("height", FRAME_HEIGHT)
+						.attr("width", FRAME_WIDTH)
+						.attr("class", "scattor-length");
+
+// Frame2: carbon emissions
+const FRAME2 = d3.select('#width')
+		          		.append('svg')
+		            		.attr('height', FRAME_HEIGHT)
+		            		.attr('width', FRAME_WIDTH)
+		            		.attr('class', 'scattor-width');
+
+// function for builidng scatter plot (Sepal_length vs. Petal_Length)
+function createvis(){
+
+	d3.csv("data/finaloutput.csv").then((data) => {
+	};
+
+// call the functions
+
