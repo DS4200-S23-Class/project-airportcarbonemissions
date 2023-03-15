8ad817e80a3b5c639df994b6aee0566c944f8cf7
danielvahey <122319959+danielvahey@users.noreply.github.com>
2023-03-15 15:21:24 -0400

    comparison numbers

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ const MARGINS = {left: 70, right: 70, top: 70, bottom: 70};
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
+// power a home for a day
+const HOME = 7.42 
+// drive one mile 
+const CAR = .35
+// eat a hamburger 
+const BURGER = 2.84
+// amount absorbed by a tree 
+const TREE = 21
+
+
 // Frame1: airport 
 const FRAME1 = d3.select("#length") 
 					.append("svg")
