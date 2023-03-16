d0214970c7d54cad69dd5cbd02e7ae3ec838d331
liangv21 <liang.va@northeastern.edu>
2023-03-16 10:02:06 -0400

    points to map

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Declare constants
-const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 700;
+const FRAME_HEIGHT = 400;
+const FRAME_WIDTH = 800;
 const MARGINS = {left: 70, right: 70, top: 70, bottom: 70};
 
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
@@ -21,37 +21,51 @@ const FRAME1 = d3.select("#airportvis")
 					.append("svg")
 						.attr("height", FRAME_HEIGHT)
 						.attr("width", FRAME_WIDTH)
-						.attr("class", "scatter-length");
+						.attr("class", "world-map");
 
 // Plot world map using d3 projection
 var projection = d3.geoNaturalEarth1()
-					.scale(VIS_HEIGHT / 2.5)
+					.scale(FRAME_WIDTH / 5.5)
     				.translate([FRAME_WIDTH / 2, FRAME_HEIGHT / 2]);;
 
 var path = d3.geoPath()
     	.projection(projection);
 
-FRAME1.selectAll('path')
-		.append('path')
-		.attr('class', 'sphere')
-		.attr('d', path({type: 'Sphere'}));
-	
-d3.json('js/maps.json')
+// Add world map to frame
+FRAME1.selectAll("path")
+		.append("path")
+		.attr("class", "sphere")
+		.attr("d", path({type: "Sphere"}));
+
+// Read maps json file to add the world map svgs
+d3.json("js/maps.json")
 	  .then(data => {
 		const countries = topojson.feature(data, data.objects.countries);
-		FRAME1.selectAll('path').data(countries.features)
+		FRAME1.selectAll("path").data(countries.features)
 		  		.enter()
-				.append('path')
-				.attr('class', 'country')
-				.attr('d', path);
+				.append("path")
+				.attr("class", "country")
+				.attr("d", path);
 	  });
 
+// Add points for each individual location
+d3.csv("data/finaloutput.csv").then((data) => {
+	FRAME1.selectAll("locations")
+			.data(data)
+			.enter()
+			.append("circle")
+				.attr("cx", function(d) { return projection([d["lon"],d["lat"]])[0];})
+				.attr("cy", function(d) { return projection([d["lon"],d["lat"]])[1];})
+				.attr("r", 2)
+				.attr("fill", "red")
+})
+
 // Frame2: carbon emissions
-const FRAME2 = d3.select('#width')
-		          		.append('svg')
-		            		.attr('height', FRAME_HEIGHT)
-		            		.attr('width', FRAME_WIDTH)
-		            		.attr('class', 'scatter-width');
+const FRAME2 = d3.select("#carbonvis")
+		          		.append("svg")
+		            		.attr("height", FRAME_HEIGHT)
+		            		.attr("width", FRAME_WIDTH)
+		            		.attr("class", "scatter-width");
 
 // function for builidng scatter plot (Sepal_length vs. Petal_Length)
 //function createvis(){
