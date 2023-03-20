37331df8aa530c0d06f176ab851608de7db5028d
liangv21 <liang.va@northeastern.edu>
2023-03-20 16:18:12 -0400

    updated js

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ const BURGER = 2.84
 // amount absorbed by a tree 
 const TREE = 21
 
+const JFK_COORDS = [40.6398, -73.7789]
+const BOS_COORDS = [42.3643, -71.0052]
+
 
 // Frame1: airport 
 // initialize background map with leaflet
@@ -31,6 +34,14 @@ L.svg().addTo(map);
 
 // add points to map with d3
 function plotMap(filter) {
+
+	if (filter == "JFK") {
+		coords = JFK_COORDS
+	} else {
+		coords = BOS_COORDS
+	};
+
+	map.setView(coords, 10)
 
 	d3.select("#airportvis")
 			.select("svg")
@@ -72,52 +83,6 @@ function changeMap() {
 }
 
 map.on("moveend", update);
-
-
-
-const FRAME1 = d3.select("#airportvis") 
-					.append("svg")
-						.attr("height", FRAME_HEIGHT)
-						.attr("width", FRAME_WIDTH)
-						.attr("class", "world-map");
-
-// Plot world map using d3 projection
-var projection = d3.geoNaturalEarth1()
-					.scale(FRAME_WIDTH / 5.5)
-    				.translate([FRAME_WIDTH / 2, FRAME_HEIGHT / 2]);;
-
-var path = d3.geoPath()
-    	.projection(projection);
-
-// Add world map to frame
-FRAME1.selectAll("path")
-		.append("path")
-		.attr("class", "sphere")
-		.attr("d", path({type: "Sphere"}));
-
-// Read maps json file to add the world map svgs
-d3.json("js/maps.json")
-	  .then(data => {
-		const countries = topojson.feature(data, data.objects.countries);
-		FRAME1.selectAll("path").data(countries.features)
-		  		.enter()
-				.append("path")
-				.attr("class", "country")
-				.attr("d", path);
-	  });
-
-// Add points for each individual location
-d3.csv("data/finaloutput.csv").then((data) => {
-	FRAME1.selectAll("locations")
-			.data(data)
-			.enter()
-			.append("circle")
-				.attr("cx", function(d) { return projection([d["lon"],d["lat"]])[0];})
-				.attr("cy", function(d) { return projection([d["lon"],d["lat"]])[1];})
-				.attr("r", 2)
-				.attr("fill", "red")
-})
-
 
 // Frame2: carbon emissions
 const FRAME2 = d3.select("#carbonvis")
