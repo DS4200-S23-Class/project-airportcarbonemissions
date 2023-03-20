e3f744d4a2a17c53730c607ad7da48605ec28d0c
liangv21 <liang.va@northeastern.edu>
2023-03-20 16:06:30 -0400

    js code

--- js/main.js
+++ js/main.js
@@ -17,6 +17,58 @@ const TREE = 21
 
 
 // Frame1: airport 
+// initialize background map with leaflet
+let map = L.map("airportvis").setView([0, 0], 1);
+
+// add tile layer with leaflet
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// add additional svg to map for d3 points to be added
+L.svg().addTo(map);
+
+// add points to map with d3
+function plotMap(filter) {
+	d3.csv("data/finaloutput.csv").then((data) => {
+
+		filteredData = data.filter(function(row) {
+			return row["from"] == filter;
+		});
+
+		console.log(filteredData);
+
+		d3.select("#airportvis")
+			.select("svg")
+			.selectAll("airportLocations")
+			.data(filteredData)
+			.enter()
+			.append("circle")
+				.attr("cx", function(d) { return map.latLngToLayerPoint([d.lat, d.lon]).x})
+				.attr("cy", function(d) { return map.latLngToLayerPoint([d.lat, d.lon]).y})
+				.attr("r", 1)
+				.attr("fill", "green")
+				.attr("fill-opacity", .8);
+})}
+
+function update() {
+	d3.selectAll("circle")
+	  .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).x })
+	  .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).y })
+	  .attr("r", map.getZoom() * 0.75);
+}
+
+function changeMap() {
+	let start_airport = document.getElementById("start-airport");
+	let selectedValue = start_airport.options[start_airport.selectedIndex].value;
+	plotMap(selectedValue)
+}
+
+map.on("moveend", update);
+
+/*
+
 const FRAME1 = d3.select("#airportvis") 
 					.append("svg")
 						.attr("height", FRAME_HEIGHT)
@@ -59,6 +111,8 @@ d3.csv("data/finaloutput.csv").then((data) => {
 				.attr("r", 2)
 				.attr("fill", "red")
 })
+
+*/
 
 // Frame2: carbon emissions
 const FRAME2 = d3.select("#carbonvis")
